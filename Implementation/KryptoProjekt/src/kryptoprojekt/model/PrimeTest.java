85f96b7833fdac8cae68f72308bde7a95c7e96d0
Ecky <Michael@.(none)>
2010-05-28 01:06:57 +0200

    PrimeTest Method (checkBases) to verify the correct base was revised a lot.
    * method 'checkBases' renamed in checkPrimeArguments
    * throws IllegalArgumentException no more
    * now also checked whether a positive number (sent to check if it is a prime number) greater than 1 was passed
    * returns a Tuple object if the arguments are correct. First Argument = boolean, second argument is a string to see what is wrong

--- Implementation/KryptoProjekt/src/kryptoprojekt/model/PrimeTest.java
+++ Implementation/KryptoProjekt/src/kryptoprojekt/model/PrimeTest.java
@@ -10,22 +10,30 @@ import java.util.*;
 public abstract class PrimeTest {
 
     //soll diese Methode vielleicht besser in die Validator Klasse?
-    //checkt ob die übergebene Basis '0 < a < Modul' ist
-    protected <E extends KryptoType<E>> boolean checkBases(List<E> bases, E checkPrime)
-            throws IllegalArgumentException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException, ClassCastException {
+    //checkt ob die übergebeneb Wert: Primzahl größer 0 und die Basis '0 < a < Modul' ist
+    protected <E extends KryptoType<E>> Tuple<Boolean, String> checkPrimeArguments(List<E> bases, E checkPrime)
+            throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException, ClassCastException {
+        try {
+            if (checkPrime.compareTo((E) Factory.newInstance(checkPrime.getClass(), "1")) <= 0) {
+                return new Tuple<Boolean, String>(false, "Es gibt nur Primzahlen >1");
+            }
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Comparison not possible. " + e.toString());
+        }
         for (E checkBases : bases) {
             try {
                 if (checkBases.compareTo((E) Factory.newInstance(checkBases.getClass(), "0")) <= 0) {
-                    throw new IllegalArgumentException("Basis zu klein. Sie muss sein:  0 < a < Modul");
+                    return new Tuple<Boolean, String> (false, "Basis zu klein. Sie muss sein:  0 < a < Modul");
                 } else if (checkBases.compareTo((E) Factory.newInstance(checkBases.getClass(), checkPrime.toString())) >= 0) {
-                    throw new IllegalArgumentException("Basis zu gro�. Sie muss sein:  0 < a < Modul");
+                    return new Tuple<Boolean, String>(false, "Basis zu groß. Sie muss sein:  0 < a < Modul");
                 }
             } catch (ClassCastException e) {
                 throw new ClassCastException("Comparison not possible. " + e.toString());
             }
         }
-        return true;
+        return new Tuple<Boolean, String>(true, "");
     }
+
 
     //Macht den eigentlichen FermatCheck. Erwartet eine Liste mit den Basen welche prüfen sollen ob die übergebene Zahl eine Primzahl ist
     protected final <E extends KryptoType<E>> boolean fermatCheck(List<E> bases, E checkPrime)
