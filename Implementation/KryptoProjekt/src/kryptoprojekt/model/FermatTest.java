036f8d2ad6cde972b095a11037834cef75901f1a
Ecky <Michael@.(none)>
2010-05-28 20:16:16 +0200

    Renamed variable 'checkBasesAnswer' and changed the return value from boolean to Tuple
    * renamed the variable 'checkBasesAnswer' to 'checkPrimeArgAnswer'
    * if-else statement check first false and then true
    * returns a Tuple object now. First Argument = boolean whether it is a prime number, second argument is the probability whether it is a prime number
    * addes a private static final probability variable which includes a factor for the probability
    * checks whether the passed list with bases at least one element contains

--- Implementation/KryptoProjekt/src/kryptoprojekt/model/FermatTest.java
+++ Implementation/KryptoProjekt/src/kryptoprojekt/model/FermatTest.java
@@ -10,17 +10,29 @@ import java.util.*;
 public final class FermatTest extends PrimeTest {
 
     private PrimeTest primeT;
-    
-    //führt den FermatTest über die abstrakte Klasse PrimeTest aus
-    public <E extends KryptoType<E>> boolean test(List<E> bases, E checkPrime)
+    //probabilityValue muss von Prof. Laschinger noch berechnet werden.
+    private static final double probabilityValue = 0.25; //Gibt die Wahrscheinlichkeit an, ob es sich um eine Primzahl handelt pro getestete Basis
+
+    /* Führt den FermatTest über die abstrakte Klasse PrimeTest aus.
+     * Die Methode test ist abstrakt in PrimeTest definiert und dort befinden sich auch die Erläuterungen zu den Übergabe- und Rückgabewerten.
+     */
+    public <E extends KryptoType<E>> Tuple<Boolean, String> test(List<E> bases, E checkPrime, int decimalPlaces)
             throws IllegalArgumentException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException, ClassCastException {
         primeT = new FermatTest();
-        //soll besser checkPrimeArguments von fermatCheck in PrimeTest aufgerufen werden? In Lucas Test muss man sonst auch vorher checkBases aufrufen
-        Tuple<Boolean, String> checkBasesAnswer = primeT.checkPrimeArguments(bases, checkPrime); //prüft ob die Basis größer 0 und kleiner ist als das Modul
-        if (checkBasesAnswer.first() == true){
-            return primeT.fermatCheck(bases, checkPrime);
-        } else{
-            throw new IllegalArgumentException(checkBasesAnswer.second());
+        //soll checkPrimeArguments besser von fermatCheck in PrimeTest aufgerufen werden? In Lucas Test muss man sonst auch vorher checkBases aufrufen
+        int numbOfElements = bases.size();
+        Tuple<Boolean, String> checkPrimeArgAnswer = null;
+        if (numbOfElements > 0) {
+            checkPrimeArgAnswer = primeT.checkPrimeArguments(bases, checkPrime); //prüft ob die Basis größer 0 und kleiner ist als das Modul
+            if (checkPrimeArgAnswer.first() == false){
+                throw new IllegalArgumentException(checkPrimeArgAnswer.second());
+            } else{
+                Boolean isPrime = primeT.fermatCheck(bases, checkPrime);
+                if (isPrime){
+                    return new Tuple<Boolean, String>(isPrime, primeT.calculateProbability(bases, probabilityValue, decimalPlaces));
+                }
+            }
         }
+        return new Tuple<Boolean, String>(false, "100%"); //Wenn keine Basen übergeben wurde, wird false zurückgegeben das es sich 100% um keine Primzahl handelt.
     }
 }
