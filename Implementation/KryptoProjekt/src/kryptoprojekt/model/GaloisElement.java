c99987aa0291f147023adcffb3d4675d70fc04d4
philipp <philipp.stussak@hs-furtwangen.de>
2010-06-02 17:03:10 +0200

    InverseElement and Division added. Exception handling in GaloisField improved.

--- Implementation/KryptoProjekt/src/kryptoprojekt/model/GaloisElement.java
+++ Implementation/KryptoProjekt/src/kryptoprojekt/model/GaloisElement.java
@@ -2,14 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package kryptoprojekt.model;
 
 /**
  *
  * @author phil
  */
-public class GaloisElement implements KryptoType<GaloisElement>{
+public class GaloisElement implements KryptoType<GaloisElement> {
 
     private Z value;
     private Z base;
@@ -20,19 +19,27 @@ public class GaloisElement implements KryptoType<GaloisElement>{
     }
 
     public GaloisElement add(GaloisElement other) {
-        if(this.base.equals(other.base))
+        if (this.base.equals(other.base)) {
             return new GaloisElement((this.value.add(other.value)).mod(base), this.base);
-        else
-            // Wrong handling of Base missmatch, corrected soon
-            return new GaloisElement(base, value);
+        } else {
+            throw new RuntimeException("Base missmatch");
+        }
     }
 
     public int compareTo(GaloisElement other) {
-        return value.compareTo(other.value);
+        if (this.base.equals(other.base)) {
+            return value.compareTo(other.value);
+        } else {
+            throw new RuntimeException("Base missmatch");
+        }
     }
 
     public GaloisElement divide(GaloisElement other) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (this.base.equals(other.base)) {
+            return this.multiply(this.inverseElement());
+        } else {
+            throw new RuntimeException();
+        }
     }
 
     public boolean isONE() {
@@ -47,20 +54,20 @@ public class GaloisElement implements KryptoType<GaloisElement>{
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public GaloisElement multiply (GaloisElement other){
-        if(this.base.equals(other.base))
+    public GaloisElement multiply(GaloisElement other) {
+        if (this.base.equals(other.base)) {
             return new GaloisElement((this.value.multiply(other.value)).mod(base), this.base);
-        else
-            // Wrong handling of Base missmatch, corrected soon
-            return new GaloisElement(base, value);
+        } else {
+            throw new RuntimeException("Base missmatch");
+        }
     }
 
     public GaloisElement subtract(GaloisElement other) {
-        if(this.base.equals(other.base))
+        if (this.base.equals(other.base)) {
             return new GaloisElement((this.value.add(this.base.subtract(other.value))).mod(base), this.base);
-        else
-            // Wrong handling of Base missmatch, corrected soon
-            return new GaloisElement(base, value);
+        } else {
+            throw new RuntimeException("Base missmatch");
+        }
     }
 
     public String toBinaryString() {
@@ -81,5 +88,9 @@ public class GaloisElement implements KryptoType<GaloisElement>{
 
     public void setValue(Z value) {
         this.value = value;
+    }
+
+    public GaloisElement inverseElement() {
+        return new GaloisElement(base.subtract(value), base);
     }
 }
