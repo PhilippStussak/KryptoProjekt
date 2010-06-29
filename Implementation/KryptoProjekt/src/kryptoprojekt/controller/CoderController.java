fcf358838fbb0d64dd4c21f26c50db441c24c13b
Mario <mario.wieser@hs-furtwangen.de>
2010-06-29 13:21:37 +0200

    further work on CoderController.java

--- Implementation/KryptoProjekt/src/kryptoprojekt/controller/CoderController.java
+++ Implementation/KryptoProjekt/src/kryptoprojekt/controller/CoderController.java
@@ -6,9 +6,12 @@
 package kryptoprojekt.controller;
 
 import java.util.HashMap;
+import javax.swing.JCheckBox;
 import kryptoprojekt.Kit;
 import kryptoprojekt.Kit.DropTextField;
 import kryptoprojekt.model.HammingCode;
+import kryptoprojekt.model.Matrix;
+import kryptoprojekt.model.PrimeFieldElement;
 
 /**
  *
@@ -22,5 +25,19 @@ public class CoderController {
 
         results.put(getTitle() + "_syndrom", result);
         return result.toString();
+    }
+
+    public static HammingCode initHammingCode(DropTextField textGeneratorMatrix, DropTextField textSourceCodeword, JCheckBox enableMarix){
+         if(enableMarix.isSelected()){
+        textGeneratorMatrix.enable(true);
+        HammingCode result = new HammingCode( (Matrix<PrimeFieldElement>)textGeneratorMatrix.getResult(),(String)textSourceCodeword.getResult());
+
+        return result;
+        }
+        else{
+        HammingCode result = new HammingCode((String)textSourceCodeword.getText());
+
+        return result;
+        }
     }
 }
